4b950be36df0b006ec029c33e8c829bc89854ae8
otaviolarrosa <otaviolarrosa@outlook.com>
2023-02-21 20:35:13 -0300

    Adjusted Consumer

--- StreamNet.PubSub/StreamNet.PubSub/Infrastructure/Consumers/OrderCreatedEventConsumer.cs
+++ StreamNet.PubSub/StreamNet.PubSub/Infrastructure/Consumers/OrderCreatedEventConsumer.cs
@@ -6,11 +6,14 @@ namespace StreamNet.Publisher.Infrastructure.Consumers;
 [ConsumerGroup("StreamNet.Tests")]
 public class OrderCreatedEventConsumer : Consumer<OrderCreatedEvent>
 {
-    private readonly ILogger<Consumer<OrderCreatedEvent>> _logger;
+    private new readonly ILogger<Consumer<OrderCreatedEvent>> _logger;
 
     public OrderCreatedEventConsumer(ILogger<Consumer<OrderCreatedEvent>> logger) : base(logger) 
         => _logger = logger;
 
-    public override async Task HandleAsync() 
-        => _logger.LogInformation("Received event {@event}.", Message);
+    public override Task HandleAsync()
+    {
+        _logger.LogInformation("Received event {@event}.", Message);
+        return Task.CompletedTask;
+    }
 }
